51dd4ff3adda6585fa71ad935003dd504b9f7658
jackfrow <>
2019-01-23 09:50:38 +0800

    1.basic.  2.string.

--- SwiftGuideDemo/SwiftGuideDemo/Collections/BasicOperatorsViewController.swift
+++ SwiftGuideDemo/SwiftGuideDemo/Collections/BasicOperatorsViewController.swift
@@ -13,18 +13,202 @@ class BasicOperatorsViewController: JKBasicViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        /* 1.赋值操作符*/
+        
+//        该赋值运算符（）初始化或更新的价值与价值：a = bab
+        let b = 10
+        var a = 5
+        a = b
+        print(a)
+        // a is now equal to 10
+        
+//        如果赋值的右侧是具有多个值的元组，则其元素可以一次分解为多个常量或变量：
+        
+        let (x, y) = (1, 2)
+        // x is equal to 1, and y is equal to 2
+//        if x = y {
+//            // This is not valid, because x = y does not return a value.
+//        }
+//        此功能可防止在实际=使用等于operator（==）时意外使用赋值运算符（）。通过使无效，Swift可以帮助您避免代码中的这类错误。if x = y
+        
+        
+//       2. 算术运算符
+          let sum =  1 + 2       // equals 3
+          let Subtraction =  5 - 3       // equals 2
+          let multiplication = 2 * 3       // equals 6
+          let division = 10.0 / 2.5  // equals 4.0
+          //print info.
+          print(sum);print(Subtraction);print(multiplication);print(division)
+        
+//       2.1 一元减法运算符
+        let three = 3
+        let minusThree = -three       // minusThree equals -3
+        let plusThree = -minusThree   // plusThree equals 3, or "minus minus three"
+        print(three);print(minusThree);print(plusThree)
+//        一元减号运算符（-）直接位于它运行的值之前，没有任何空格。
+        
+//       2.2 一元加运算符
+        let minusSix = -6
+        let alsoMinusSix = +minusSix  // alsoMinusSix equals -6
+//        尽管一元加运算符实际上并没有做任何事情，但是当使用一元减号运算符作为负数时，您可以使用它来为代码提供正数的对称性。
+        
+        
+        //3. 复合赋值运算符
+//        与C一样，Swift提供了将assignment（）与另一个操作相结合的复合赋值运算符=。一个例子是加法赋值运算符（+=）：
+        var complex = 1
+        complex += 2
+        // a is now equal to 3
+
+        //4. 比较运算符
+       /* Swift支持所有标准C 比较运算符：
+        
+        等于（）a == b
+        不等于（）a != b
+        大于（）a > b
+        小于（）a < b
+        大于或等于（）a >= b
+        小于或等于（）a <= b
+        Swift还提供了两个标识运算符（===和!==），您可以使用它们来测试两个对象引用是否都引用同一个对象实例。有关更多信息，请参阅结构和类。
+        */
+        
+//        每个比较运算符都返回一个Bool值，以指示该语句是否为true：
+      /*  1 == 1   // true because 1 is equal to 1
+        2 != 1   // true because 2 is not equal to 1
+        2 > 1    // true because 2 is greater than 1
+        1 < 2    // true because 1 is less than 2
+        1 >= 1   // true because 1 is greater than or equal to 1
+        2 <= 1   // false because 2 is not less than or equal to 1 */
+        
+//        比较运算符通常用于条件语句，例如if语句：
+        let name = "world"
+        if name == "world" {
+            print("hello, world")
+        } else {
+            print("I'm sorry \(name), but I don't recognize you")
+        }
+        // Prints "hello, world", because name is indeed equal to "world".
+        
+//        如果它们具有相同的类型和相同数量的值，则可以比较两个元组。元组从左到右进行比较，一次一个值，直到比较发现两个不相等的值。比较这两个值，并且该比较的结果确定元组比较的总体结果。如果所有元素都相等，则元组本身是相等的。例如：
+        (1, "zebra") < (2, "apple")   // true because 1 is less than 2; "zebra" and "apple" are not compared
+        (3, "apple") < (3, "bird")    // true because 3 is equal to 3, and "apple" is less than "bird"
+        (4, "dog") == (4, "dog")      // true because 4 is equal to 4, and "dog" is equal to "dog"
+        
+//        5.三元条件算子
+//        所述三元条件算子是由三个部分组成，这需要形式的特殊操作。它是根据是真还是假来评估两个表达式之一的快捷方式。如果为true，则计算并返回其值; 否则，它会评估并返回其值。question ? answer1 : answer2questionquestionanswer1answer2
+//        三元条件运算符是以下代码的简写：
+        
+//        if question {
+//            answer1
+//        } else {
+//            answer2
+//        }
+        
+        
+        //6.Nil-Coalescing运算符
+//        表达式必须与存储在其中的类型匹配。a ?? babanilaba
+//        nil-coalescing运算符是下面代码的简写：
+//        a != nil ? a! : b
+        
+        
+//       7. 范围运算符
+//       7.1 闭区域操作符
+        for index in 1...5 {
+            print("\(index) times 5 is \(index * 5)")
+        }
+        
+        //7.2 半开放式操作符
+        let names = ["Anna", "Alex", "Brian", "Jack"]
+        let count = names.count
+        for i in 0..<count {
+            print("Person \(i + 1) is called \(names[i])")
+        }
+        
+        //7.3 单面范围
+        for name in names[2...] {
+            print(name)
+        }
+        // Brian
+        // Jack
+        
+        for name in names[...2] {
+            print(name)
+        }
+        // Anna
+        // Alex
+        // Brian
+        
+        for name in names[..<2] {
+            print(name)
+        }
+        // Anna
+        // Alex
+        
+//        单边范围可以在其他上下文中使用，而不仅仅在下标中使用。您不能迭代省略第一个值的单侧范围，因为不清楚迭代应该从哪里开始。您可以迭代忽略其最终值的单侧范围; 但是，因为范围无限期地继续，所以请确保为循环添加显式结束条件。您还可以检查单侧范围是否包含特定值，如下面的代码所示。
+        let range = ...5
+        range.contains(7)   // false
+        range.contains(4)   // true
+        range.contains(-1)  // true
+        
+        
+        //8.逻辑运算符
+        /*
+         逻辑运算符修改或组合布尔逻辑值true和false。Swift支持基于C语言的三个标准逻辑运算符：
+         
+         逻辑NOT（!a）
+         逻辑AND（）a && b
+         逻辑OR（）a || b
+         */
+        
+        //8.1 逻辑NOT运算符
+//        逻辑NOT运算符是前缀运算符，它出现在它运行的值之前，没有任何空格。它可以读作“not a”，如下例所示：
+        let allowedEntry = false
+        if !allowedEntry {
+            print("ACCESS DENIED")
+        }
+        // Prints "ACCESS DENIED"
+        
+        //8.2 逻辑AND运算符
+//        此示例考虑两个Bool值，并且只有两个值都允许访问true：
+        let enteredDoorCode = true
+        let passedRetinaScan = false
+        if enteredDoorCode && passedRetinaScan {
+            print("Welcome!")
+        } else {
+            print("ACCESS DENIED")
+        }
+        // Prints "ACCESS DENIED"
+        
+        //8.3 逻辑OR运算符
+//        在下面的示例中，第一个Bool值（hasDoorKey）是false，但第二个值（knowsOverridePassword）是true。因为一个值是true，整个表达式也计算为true，并允许访问：
+        let hasDoorKey = false
+        let knowsOverridePassword = true
+        if hasDoorKey || knowsOverridePassword {
+            print("Welcome!")
+        } else {
+            print("ACCESS DENIED")
+        }
+        // Prints "Welcome!"
+        
+        
+        //8.4 结合逻辑运算符
+        if enteredDoorCode && passedRetinaScan || hasDoorKey || knowsOverridePassword {
+            print("Welcome!")
+        } else {
+            print("ACCESS DENIED")
+        }
+        // Prints "Welcome!"
+        
+        //8.5 明确的括号
+        if (enteredDoorCode && passedRetinaScan) || hasDoorKey || knowsOverridePassword {
+            print("Welcome!")
+        } else {
+            print("ACCESS DENIED")
+        }
+        // Prints "Welcome!"
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+ 
 
 }
--- SwiftGuideDemo/SwiftGuideDemo/Collections/StringsandCharactersViewController.swift
+++ SwiftGuideDemo/SwiftGuideDemo/Collections/StringsandCharactersViewController.swift
@@ -13,18 +13,100 @@ class StringsandCharactersViewController: JKBasicViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //1.字符串文字
+        //1.1 使用字符串文字作为常量或变量的初始值：
+        let someString = "Some string literal value"
+        print(someString)
+        //1.2 多行字符串文字 : 如果需要跨越多行的字符串，请使用多行字符串文字 - 由三个双引号括起来的字符序列：
+        let quotation = """
+The White Rabbit put on his spectacles.  "Where shall I begin,
+please your Majesty?" he asked.
+
+"Begin at the beginning," the King said gravely, "and go on
+till you come to the end; then stop."
+"""
+        print(quotation)
+        
+        //1.3 字符串文字中的特殊字符
+       /* 字符串文字可以包含以下特殊字符：
+        
+        转义的特殊字符\0（空字符），\\（反斜杠），\t（水平制表符），\n（换行符），\r（回车符），\"（双引号）和\'（单引号）
+        任意的Unicode标值，写为\u{Ñ}，其中Ñ是一个1-8位十六进制数（统一在讨论的Unicode下文）*/
+        
+        
+        //2. 初始化空字符串
+//        要创建空String值作为构建更长字符串的起点，请将空字符串文字指定给变量，或String使用初始化程序语法初始化新实例：
+        var emptyString = ""               // empty string literal
+        var anotherEmptyString = String()  // initializer syntax
+        
+        //String通过检查其布尔isEmpty属性来确定值是否为空：
+        if emptyString.isEmpty {
+            print("Nothing to see here")
+        }
+        // Prints "Nothing to see here"
+        
+        
+        //3.字符串可变性
+        var variableString = "Horse"
+        variableString += " and carriage"
+        print(variableString)
+        
+//        let constantString = "Highlander"
+//        constantString += " and another Highlander"
+        // this reports a compile-time error - a constant string cannot be modified
+        
+        //4.字符串是值类型
+        /*Swift的String类型是值类型。如果创建新String值，则在将String值传递给函数或方法时，或者将其赋值给常量或变量时，将复制该值。在每种情况下，String都会创建现有值的新副本，并传递或分配新副本，而不是原始版本。结构和枚举是值类型中描述了值类型。*/
+        
+        
+        //5.Working with Characters
+       // 5.1. You can access the individual Character values for a String by iterating over the string with a for-in loop:
+        for character in "Dog!🐶" {
+            print(character)
+        }
+        //5.2 String可以通过将Character值数组作为参数传递给其初始值设定项来构造值：
+        let catCharacters: [Character] = ["C", "a", "t", "!", "🐱"]
+        let catString = String(catCharacters)
+        print(catString)
+        
+        //6.连接字符串和字符
+//       6.1 String值可以与加法运算符（）一起添加（或连接+）以创建新String值：
+        let string1 = "hello"
+        let string2 = " there"
+        var welcome = string1 + string2
+        print(welcome)
+        // welcome now equals "hello there"
+//        6.2 您还可以使用加号赋值运算符（）将String值附加到现有String变量+=：
+        var instruction = "look over"
+        instruction += string2
+        // instruction now equals "look over there"
+        //6.3 您可以使用类型的方法将Character值附加到String变量：Stringappend()
+        let exclamationMark: Character = "!"
+        welcome.append(exclamationMark)
+        // welcome now equals "hello there!"
+        //6.4 如果您使用多行字符串文字来构建较长字符串的行，则希望字符串中的每一行都以换行符结束，包括最后一行。例如：
+        let badStart = """
+one
+two
+"""
+        let end = """
+three
+"""
+        print(badStart + end)
+        
+        
+        let goodStart = """
+one
+two
+
+"""
+
+        print(goodStart + end)
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
